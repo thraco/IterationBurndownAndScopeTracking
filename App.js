24efada9eeba77f52546c82854ea7f9cd4a6a79a
ted <tcopelan@dhcp73-145.rdu.redhat.com>
2018-07-20 14:07:19 -0400

    Reformatting to use consistent indentation size

--- App.js
+++ App.js
@@ -1,138 +1,138 @@
 Ext.define('Iteration_BurndownApp', {
-    extend: 'Rally.app.TimeboxScopedApp',
-    scopeType: 'iteration',
-    _burndownChartID: 'burndownChart',
+  extend: 'Rally.app.TimeboxScopedApp',
+  scopeType: 'iteration',
+  _burndownChartID: 'burndownChart',
 
-    launch: function() {
-        //Write app code here
-        //API Docs: https://help.rallydev.com/apps/2.1/doc/
+  launch: function() {
+    //Write app code here
+    //API Docs: https://help.rallydev.com/apps/2.1/doc/
 
-        this.add({
-          xtype: 'rallycheckboxfield',
-          fieldLabel: 'Scope in Story Points',
-          id: 'byStoryPointCheckBox',
-          value: false,
-          listeners: {
-            change: {fn: this.onScopeChange, scope: this}
-          }
-        });
-
-        this.onScopeChange();
-    },
-
-    onScopeChange: function() {
-      var capacity = this._loadCapacity();
-
-      capacity.load().then({
-        success: this._loadChart,
-        scope: this
-      }).then({
-        success: function() {
-          console.log('Woohoo!');
-        },
-        failure: function(error) {
-          console.log('ERROR! '+error);
-        }
-      });
-    },
-
-    _loadCapacity: function() {
-      return Ext.create('Rally.data.wsapi.Store', {
-        model: 'UserIterationCapacity',
-        fetch: ['Capacity', 'Iteration'],
-        pageSize: 1000, //TODO: Figure out how to limit the query to the timebox scope
-      });
-    },
-
-    _loadChart: function (capacity) {
-      var scope = this.getContext().getTimeboxScope();
-      var burnByCount = this.down('#byStoryPointCheckBox').getValue() === false;
-
-      if(this.down('#burndownChart')) {
-        this.remove('burndownChart');
+    this.add({
+      xtype: 'rallycheckboxfield',
+      fieldLabel: 'Scope in Story Points',
+      id: 'byStoryPointCheckBox',
+      value: false,
+      listeners: {
+        change: {fn: this.onScopeChange, scope: this}
       }
+    });
 
-      var totalCap = 0;
-      for(i=0; i<capacity.length; i++) {
-        if(capacity[i].get('Iteration')._refObjectName === scope.record.data.Name) {
-          var cap = capacity[i].get('Capacity');
-          if(cap != null) {
-            totalCap += cap;
-          }
+    this.onScopeChange();
+  },
+
+  onScopeChange: function() {
+    var capacity = this._loadCapacity();
+
+    capacity.load().then({
+      success: this._loadChart,
+      scope: this
+    }).then({
+      success: function() {
+        // Success!
+      },
+      failure: function(error) {
+        console.log('ERROR! '+error);
+      }
+    });
+  },
+
+  _loadCapacity: function() {
+    return Ext.create('Rally.data.wsapi.Store', {
+      model: 'UserIterationCapacity',
+      fetch: ['Capacity', 'Iteration'],
+      pageSize: 1000, //TODO: Figure out how to limit the query to the timebox scope
+    });
+  },
+
+  _loadChart: function (capacity) {
+    var scope = this.getContext().getTimeboxScope();
+    var burnByCount = this.down('#byStoryPointCheckBox').getValue() === false;
+
+    if(this.down('#burndownChart')) {
+      this.remove('burndownChart');
+    }
+
+    var totalCap = 0;
+    for(i=0; i<capacity.length; i++) {
+      if(capacity[i].get('Iteration')._refObjectName === scope.record.data.Name) {
+        var cap = capacity[i].get('Capacity');
+        if(cap != null) {
+          totalCap += cap;
         }
       }
+    }
 
-      this.add({
-        xtype: 'rallychart',
-        id: 'burndownChart',
-        storetype: 'Rally.data.lookback.SnapshotStore',
-        storeConfig: this._getStoreConfig(scope),
-        calculatorType: 'IterationBurndownCalculator',
-        calculatorConfig: {
-          completedScheduleStateNames: ['Accepted', 'Quality'],
-          startDate: scope.record.data.StartDate,
-          endDate: scope.record.data.EndDate,
-          capacity: totalCap,
-          metrics: [
-            {
-              as: 'ToDo',
-              display: 'line',
-              f: 'sum',
-              field: 'TaskRemainingTotal'
-            },
-            {
-              as: 'Scope',
-              display: 'area',
-              f: (burnByCount)? 'count' : 'sum',
-              yAxis: 1,
-              field: (burnByCount)? null : 'PlanEstimate'
-            }
-          ]
-        },
-        chartConfig: this._getChartConfig(burnByCount)
-      });
-    },
-
-    _getStoreConfig: function (scope) {
-      return [
-        {
-          find: {
-            "$or": [
-              {"_TypeHierarchy": "HierarchicalRequirement"},
-              {"_TypeHierarchy": "Defect"}
-            ],
-            'Iteration' : scope.record.data.ObjectID
-          },
-          fetch: ["TaskRemainingTotal", "PlanEstimate"]
-        }
-      ]
-    },
-
-    _getChartConfig: function(burnByCount) {
-      var config = {
-        title: {
-          text: 'Iteration Burndown'
-        },
-        xAxis: {
-          tickmarkPlacement: 'on'
-        },
-        yAxis: [
+    this.add({
+      xtype: 'rallychart',
+      id: 'burndownChart',
+      storetype: 'Rally.data.lookback.SnapshotStore',
+      storeConfig: this._getStoreConfig(scope),
+      calculatorType: 'IterationBurndownCalculator',
+      calculatorConfig: {
+        completedScheduleStateNames: ['Accepted', 'Quality'],
+        startDate: scope.record.data.StartDate,
+        endDate: scope.record.data.EndDate,
+        capacity: totalCap,
+        metrics: [
           {
-            title: {text: 'Hours'},
-            min: 0
+            as: 'ToDo',
+            display: 'line',
+            f: 'sum',
+            field: 'TaskRemainingTotal'
+          },
+          {
+            as: 'Scope',
+            display: 'area',
+            f: (burnByCount)? 'count' : 'sum',
+            yAxis: 1,
+            field: (burnByCount)? null : 'PlanEstimate'
           }
-        ],
-        chart: {
-          zoomType: 'xy'
-        }
-      }
+        ]
+      },
+      chartConfig: this._getChartConfig(burnByCount)
+    });
+  },
 
-      if(!burnByCount) {
+  _getStoreConfig: function (scope) {
+    return [
+      {
+        find: {
+          "$or": [
+            {"_TypeHierarchy": "HierarchicalRequirement"},
+            {"_TypeHierarchy": "Defect"}
+          ],
+          'Iteration' : scope.record.data.ObjectID
+        },
+        fetch: ["TaskRemainingTotal", "PlanEstimate"]
+      }
+    ]
+  },
+
+  _getChartConfig: function(burnByCount) {
+    var config = {
+      title: {
+        text: 'Iteration Burndown'
+      },
+      xAxis: {
+        tickmarkPlacement: 'on'
+      },
+      yAxis: [
+        {
+          title: {text: 'Hours'},
+          min: 0
+        }
+      ],
+      chart: {
+        zoomType: 'xy'
+      }
+    }
+
+    if(!burnByCount) {
       config.yAxis.push({title: { text: 'Story Points'}, opposite: true});
     } else {
       config.yAxis.push({title: {text: 'Story Count'}, opposite: true});
     }
 
-      return config
-    }
+    return config
+  }
 });
--- Reports/BurndownCalculator.js
+++ Reports/BurndownCalculator.js
@@ -62,13 +62,14 @@ Ext.define('IterationBurndownCalculator', {
         },
         display: 'line',
         dashStyle: 'shortdash',
-        hidden: true
+        visible: false
       }
     ]
   },
+
   runCalculation: function(snapshots) {
     const tomorrow =  Ext.Date.format(new Date(new Date().getTime() + 86400000), "Y-m-d\\TH:i:s") + '.000Z';
-    // snapthots that extend to the default date are 'current' so we should set their 'ValidTo' property to tomorrow's date.
+    // snapshots that extend to the default date are 'current' so we should set their 'ValidTo' property to tomorrow's date.
     // this prevents the Actual burndown from flatlining into the future, and forces it to only report the past and current state of things.
     Ext.Array.forEach(snapshots, function(s) {
       if(s._ValidTo === '9999-01-01T00:00:00.000Z') {
@@ -77,7 +78,7 @@ Ext.define('IterationBurndownCalculator', {
     }, this);
 
     // TODO want to simply call the parent function rather than duplicating its logic below.
-  //  reutrn IterationBurndownCalculator.superclass.runCalculation(snapshots);
+    //  return IterationBurndownCalculator.superclass.runCalculation(snapshots);
 
     var calculatorConfig = this._prepareCalculatorConfig(),
     seriesConfig = this._buildSeriesConfig(calculatorConfig);
@@ -87,12 +88,13 @@ Ext.define('IterationBurndownCalculator', {
 
     return this._transformLumenizeDataToHighchartsSeries(calculator, seriesConfig);
   },
-// projections aren't working the way I've intented for now, and don't have time to address so disabling.
+
+// projections aren't working the way I've intended for now, and don't have time to address so disabling.
 // may want to manually implement a projection in the getDerivedFieldsAfterSummary method instead of fighting with this one.
   getProjectionsConfig: function () {
     return {
       continueWhile: function(point) {
-        return point.ToDo_projectoin > 0;
+        return point.ToDo_projection > 0;
       },
       series: [
         {field: 'ToDo'}
@@ -101,49 +103,49 @@ Ext.define('IterationBurndownCalculator', {
   },
 
   _transformLumenizeDataToHighchartsSeries: function (calculator, seriesConfig) {
-      var results = calculator.getResults();
+    var results = calculator.getResults();
 
-      var seriesData = results.seriesData;
-      seriesData = this._calculateMaximumBurndown(seriesData);
+    var seriesData = results.seriesData;
+    seriesData = this._calculateMaximumBurndown(seriesData);
 
-      var finalData = {
-          series: this.lumenize.arrayOfMaps_To_HighChartsSeries(seriesData, seriesConfig),
-          categories: this._buildCategoriesFromData(seriesData)
-      };
+    var finalData = {
+      series: this.lumenize.arrayOfMaps_To_HighChartsSeries(seriesData, seriesConfig),
+      categories: this._buildCategoriesFromData(seriesData)
+    };
 
-      if (this.enableProjections) {
-          finalData.projections = results.projections;
-      }
+    if (this.enableProjections) {
+      finalData.projections = results.projections;
+    }
 
-      return finalData;
+    return finalData;
   },
 
   _buildSeriesConfig: function (calculatorConfig) {
-     var aggregationConfig = [],
-         metrics = calculatorConfig.metrics,
-         derivedFieldsAfterSummary = calculatorConfig.deriveFieldsAfterSummary;
+    var aggregationConfig = [],
+      metrics = calculatorConfig.metrics,
+      derivedFieldsAfterSummary = calculatorConfig.deriveFieldsAfterSummary;
 
-     for (var i = 0, ilength = metrics.length; i < ilength; i += 1) {
-         var metric = metrics[i];
-         aggregationConfig.push({
-             name: metric.as || metric.field,
-             type: metric.display,
-             dashStyle: metric.dashStyle || "Solid",
-             yAxis: metric.yAxis || 0
-         });
-     }
+    for (var i = 0, ilength = metrics.length; i < ilength; i += 1) {
+      var metric = metrics[i];
+      aggregationConfig.push({
+        name: metric.as || metric.field,
+        type: metric.display,
+        dashStyle: metric.dashStyle || "Solid",
+        yAxis: metric.yAxis || 0
+      });
+    }
 
-     for (var j = 0, jlength = derivedFieldsAfterSummary.length; j < jlength; j += 1) {
-         var derivedField = derivedFieldsAfterSummary[j];
-         aggregationConfig.push({
-             name: derivedField.as,
-             type: derivedField.display,
-             dashStyle: derivedField.dashStyle || "Solid",
-             yAxis: derivedField.yAxis || 0
-         });
-     }
+    for (var j = 0, jlength = derivedFieldsAfterSummary.length; j < jlength; j += 1) {
+      var derivedField = derivedFieldsAfterSummary[j];
+      aggregationConfig.push({
+        name: derivedField.as,
+        type: derivedField.display,
+        dashStyle: derivedField.dashStyle || "Solid",
+        yAxis: derivedField.yAxis || 0
+      });
+    }
 
-     return aggregationConfig;
+    return aggregationConfig;
   },
 
   _calculateMaximumBurndown: function(seriesData) {
